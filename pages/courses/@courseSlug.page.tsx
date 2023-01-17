a203c40057e5ca5cac49ecc47153211d90413d63
AlexONeillTI <alex.oneill@thoughtindustries.com>
2023-01-17 10:02:52 +0000

    add new helium demo

---
+++ pages/courses/@courseSlug.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export { Page };
+
+const Page = () => {
+  return (
+    <div className="text-center pt-20">
+      <div>This is a Helium Page!</div>
+    </div>
+  );
+};
